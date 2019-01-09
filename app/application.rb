b493c5b10c1d2440a96c9aa0364315631af3077f
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-09 08:53:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-      item_title = req.path.split("/items/").last
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|item}
     end
   end
 end
