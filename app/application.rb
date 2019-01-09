f2df58e7a9a131e5df7e8e0f51a977046bc09c46
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-09 08:51:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-      item = req.path.split("/items/")
+      item = req.path.split("/items/").last
     end
   end
 end
