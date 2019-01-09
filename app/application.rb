a7f85d8264b017e59284d6d78055f9276afdcba8
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-09 08:54:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,8 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
+    else
+      resp.write
     end
   end
 end
